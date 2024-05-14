1f97c56bf50222607ee40172a154c9b25e54dfff
cmatthew-s <84705250+cmatthew-s@users.noreply.github.com>
2024-05-14 07:34:08 +0700

    rebranding

--- src/routes/components/Navbar.svelte
+++ src/routes/components/Navbar.svelte
@@ -4,7 +4,7 @@
     <div class="flex flex-wrap justify-between items-center mx-auto p-4 px-20">
         <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="logo.png" class="h-8" alt="Flowbite Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap text-light">Devon</span>
+            <span class="self-center text-2xl font-semibold whitespace-nowrap text-light">Matthew</span>
         </a>
         <div class="flex items-center space-x-6 rtl:space-x-reverse">
             <a href="tel:5541251234" class="text-sm hover:underline text-light">cmatthewsusanto@gmail.com</a>
